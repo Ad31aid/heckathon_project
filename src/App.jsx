7e4ba5952f1632e452ea86ca5d1392cbd2beaf99
Ad31ide <seanchendu@gmail.com>
2024-02-18 11:47:52 -0600

    messager

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "sk-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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to new college studet who needs help with course selection."
